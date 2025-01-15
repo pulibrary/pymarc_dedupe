03e3579727fc3d989f916f9c44177f8342ade691
Max Kadel <mkadel@gmail.com>
2025-01-15 13:43:29 -0500

    Name CSV output file based on input xml file name (#14)

--- src/marc_to_csv.py
+++ src/marc_to_csv.py
@@ -1,14 +1,17 @@
 import csv
+import os.path
 from pymarc import parse_xml_to_array
 from src.marc_record import MarcRecord
 
 
 class MarcToCsv:
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, input_file_path):
+        self.input_file_path = input_file_path
 
     def to_csv(self):
-        with open("marc_xml_as_csv.csv", "w", newline="", encoding="utf-8") as csvfile:
+        filename, _file_extension = os.path.splitext(self.input_file_path)
+        output_file = filename + ".csv"
+        with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
             fieldnames = self.records()[0].keys()
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
@@ -16,7 +19,7 @@ class MarcToCsv:
                 writer.writerow(record)
 
     def pymarc_records_from_file(self):
-        return parse_xml_to_array(self.file_path)
+        return parse_xml_to_array(self.input_file_path)
 
     def records(self):
         list_of_record_dictionaries = []
