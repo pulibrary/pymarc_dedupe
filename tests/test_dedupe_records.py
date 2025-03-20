542957755b5ba3eab6560cebf8e0a30eca128ef4
Max Kadel <mkadel@princeton.edu>
2025-03-20 11:37:11 -0400

    Increase test coverage

--- tests/test_dedupe_records.py
+++ tests/test_dedupe_records.py
@@ -21,3 +21,20 @@ def test_with_files_already_created():
     my_class = DedupeRecords(test_file, output_directory)
     model = my_class.deduper()
     my_class.cluster(model)
+
+
+# pylint: disable=duplicate-code
+@patch("builtins.input")
+def test_prepare_training_data(mocker, helpers, console_inputs):
+    mocker.side_effect = console_inputs
+    files = [
+        "tests/test_outputs/data_matching_learned_settings",
+        "tests/test_outputs/data_matching_output.csv",
+    ]
+    helpers.file_cleanup(files)
+    test_file = "tests/fixtures/alma_marc_records_short.xml"
+    output_directory = "tests/test_outputs"
+    my_class = DedupeRecords(test_file, output_directory)
+    model = my_class.deduper()
+    my_class.cluster(model)
+    # pylint: enable=duplicate-code
