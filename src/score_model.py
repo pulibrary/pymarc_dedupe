7f3253f0507f2c451840fdc913a6a95cecc43b73
Max Kadel <mkadel@princeton.edu>
2025-03-25 08:59:17 -0400

    Play with scoring, try to see why so many false negatives

--- src/score_model.py
+++ src/score_model.py
@@ -4,11 +4,11 @@ from src.confusion_matrix import ConfusionMatrix
 
 class ScoreModel:
     def __init__(self, model_csv, labels_csv):
-        model_data = self.read_model_data(model_csv)
-        labels_data = self.read_labels_data(labels_csv)
-        matrix_obj = ConfusionMatrix(labels_data, model_data)
+        self.model_data = self.read_model_data(model_csv)
+        self.labels_data = self.read_labels_data(labels_csv)
+        matrix_obj = ConfusionMatrix(self.labels_data, self.model_data)
         self.confusion_matrix = ConfusionMatrix(
-            labels_data, model_data
+            self.labels_data, self.model_data
         ).confusion_matrix
         self.tp = matrix_obj.tp()
         self.tn = matrix_obj.tn()
@@ -20,7 +20,8 @@ class ScoreModel:
         with open(model_csv, encoding="utf-8") as f:
             reader = csv.DictReader(f)
             for row in reader:
-                data_d[row["id"]] = dict([("cluster_id", row["Cluster ID"])])
+                # data_d[row["id"]] = dict([("cluster_id", row["Cluster ID"])])
+                data_d[row["id"]] = dict(row)
         return data_d
 
     def read_labels_data(self, labels_csv):
@@ -40,8 +41,34 @@ class ScoreModel:
     def precision(self):
         return (self.tp) / (self.tp + self.fp)
 
+    # Same as recall
     def sensitivity(self):
         return (self.tp) / (self.tp + self.fn)
 
     def specificity(self):
         return (self.tn) / (self.tn + self.fp)
+
+    def write_false_negatives_to_file(self):
+        output_file = 'experiments_files_and_output/false_negatives.csv'
+        with open(output_file, "w", newline="", encoding="utf-8") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self.model_data.get('id.6d631bb460').keys())
+            writer.writeheader()
+            for label_row in self.labels_data:
+                first_model_row = self.model_data[label_row["id1"]]
+                second_model_row = self.model_data[label_row["id2"]]
+                first_cluster_id = first_model_row["Cluster ID"]
+                second_cluster_id = second_model_row["Cluster ID"]
+                label = int(label_row["label"])
+                model_guess = self.model_guess(first_cluster_id, second_cluster_id)
+                if label != model_guess and label == 1:
+                    writer.writerow(first_model_row)
+                    writer.writerow(second_model_row)
+
+
+    def model_guess(self, first_cluster_id, second_cluster_id):
+        guess = None
+        if first_cluster_id == second_cluster_id:
+            guess = 1
+        else:
+            guess = 0
+        return guess
--- tests/test_score_model.py
+++ tests/test_score_model.py
@@ -2,17 +2,18 @@ from src.score_model import ScoreModel
 
 
 def test_scoring():
-    model_output_path = "tests/fixtures/train_short_output.csv"
-    labels_path = "tests/fixtures/train_short_labels.csv"
+    model_output_path = "experiments_files_and_output/large_data_set_train/data_matching_output.csv"
+    labels_path = "experiments_files_and_output/large_data_set_train/202303_goldfinch_set_1.1_train_labels.csv"
     scorer = ScoreModel(model_output_path, labels_path)
-    assert (scorer.confusion_matrix) == {
-        "true positive": 23,
-        "true negative": 25,
-        "false positive": 0,
-        "false negative": 2,
-    }
-    assert (scorer.accuracy()) == 0.96
-    assert (scorer.misclassification()) == 0.04
-    assert (scorer.precision()) == 1.0
-    assert (scorer.sensitivity()) == 0.92
-    assert (scorer.specificity()) == 1.0
+    # assert (scorer.confusion_matrix) == {
+    #     "true positive": 23,
+    #     "true negative": 25,
+    #     "false positive": 0,
+    #     "false negative": 2,
+    # }
+    # assert (scorer.accuracy()) == 0.96
+    # assert (scorer.misclassification()) == 0.04
+    # assert (scorer.precision()) == 1.0
+    # assert (scorer.sensitivity()) == 0.92
+    # assert (scorer.specificity()) == 1.0
+    scorer.write_false_negatives_to_file()
